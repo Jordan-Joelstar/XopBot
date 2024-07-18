597e30006773d98a16754a8dda27d4829da40996
Astro (Daniel) <danielfx011@gmail.com>
2024-07-18 18:25:31 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,31 +5,31 @@ const long = String.fromCharCode(8206);
 const readmore = long.repeat(4001);
 
 bot(
- {
-  pattern: "menu",
-  desc: "Show All Commands",
-  dontAddCommandList: true,
- },
- async (message, input) => {
-  try {
-   const { commands } = require("../lib");
-   const categorizedCommands = {};
-   commands.forEach((command) => {
-    if (command.dontAddCommandList === false && command.pattern !== undefined) {
-     if (!categorizedCommands[command.category]) {
-      categorizedCommands[command.category] = [];
-     }
-     categorizedCommands[command.category].push(command.pattern);
-    }
-   });
+  {
+    pattern: "menu",
+    desc: "Show All Commands",
+    dontAddCommandList: true,
+  },
+  async (message, input) => {
+    try {
+      const { commands } = require("../lib");
+      const categorizedCommands = {};
+      commands.forEach((command) => {
+        if (command.dontAddCommandList === false && command.pattern !== undefined) {
+          if (!categorizedCommands[command.category]) {
+            categorizedCommands[command.category] = [];
+          }
+          categorizedCommands[command.category].push(command.pattern);
+        }
+      });
 
-   const currentTime = message.time;
-   const currentDate = message.date;
-   const currentUser = message.sender.split("@")[0];
-   let menuText = `
+      const currentTime = message.time;
+      const currentDate = message.date;
+      const currentUser = message.sender;
+      let menuText = `
 ╭═「 ${config.botname || "xᴘᴏ ᴍᴅ"} 」
 ┃╭──────────────
-┃│ ᴜsᴇʀ : ${currentUser}
+┃│ ᴜsᴇʀ : @${currentUser}
 ┃│ ᴛɪᴍᴇ : ${currentTime}
 ┃│ ᴅᴀᴛᴇ : ${currentDate}
 ┃│ ʀᴀᴍ  : ${formatp(os.totalmem() - os.freemem())}
@@ -41,29 +41,29 @@ bot(
 \t *ᴠᴇʀsɪᴏɴ 𝟷.𝟶*
 ${readmore}`;
 
-   // Append commands to the menu text
-   for (const category in categorizedCommands) {
-    menuText += `
+      // Append commands to the menu text
+      for (const category in categorizedCommands) {
+        menuText += `
 「 *${tiny(category)}* 」\n`;
 
-    for (const command of categorizedCommands[category]) {
-     menuText += `││◦ ${tiny(command, 1)}\n`;
+        for (const command of categorizedCommands[category]) {
+          menuText += `││◦ ${tiny(command, 1)}\n`;
+        }
+
+        menuText += `│╰────────────┈⊷\n╰─────────────┈⊷\n`;
+
+        // If input matches the category, break after appending its commands
+        if (input.toLowerCase() === category.toLowerCase()) {
+          break;
+        }
+      }
+      const messageOptions = {
+        caption: menuText,
+      };
+
+      return await message.sendUi(message.chat, messageOptions, message);
+    } catch (error) {
+      await message.error(`${error}\nCommand: menu`, error);
     }
-
-    menuText += `│╰────────────┈⊷\n╰─────────────┈⊷\n`;
-
-    // If input matches the category, break after appending its commands
-    if (input.toLowerCase() === category.toLowerCase()) {
-     break;
-    }
-   }
-   const messageOptions = {
-    caption: menuText,
-   };
-
-   return await message.sendUi(message.chat, messageOptions, message);
-  } catch (error) {
-   await message.error(`${error}\nCommand: menu`, error);
-  }
- }
+  },
 );
