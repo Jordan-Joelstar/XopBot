0ba44114187fa395dc90281959f438a25673b705
Astro (Daniel) <danielfx011@gmail.com>
2024-07-18 22:41:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const bot = require('./lib/client')
-const { VERSION } = require('./config')
-
-const start = async () => {
-    Debug.info(`Version ${VERSION}`)
-  try {
-    await bot.init()
-    bot.logger.info('⏳ Database syncing!')
-    await bot.DATABASE.sync()
-    await bot.connect()
-  } catch (error) {
-    Debug.error(error);
-    start();
-  }
-}
-start();
+const bot=require("./lib/client"),{VERSION:VERSION}=require("./config"),start=async()=>{Debug.info(`Version ${VERSION}`);try{await bot.init(),bot.logger.info("⏳ Database syncing!"),await bot.DATABASE.sync(),await bot.connect()}catch(t){Debug.error(t),start()}};start();
