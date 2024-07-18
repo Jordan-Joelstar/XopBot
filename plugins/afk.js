8b4d250b955f9def7aa5ec0ec337d361928c9fb2
Astro (Daniel) <danielfx011@gmail.com>
2024-07-18 14:29:06 +0200

    afk

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,87 +1,151 @@
-const { bot } = require("../lib/");
+const { bot } = require("../lib/index");
+const fs = require("fs-extra");
 
-var AFK = {
- isAfk: false,
- reason: false,
- lastseen: 0,
+let db = {};
+
+db.get = async () => {
+ const filePath = "./afk.json";
+ try {
+  return JSON.parse(fs.readFileSync(filePath, "utf-8"));
+ } catch (error) {
+  return {};
+ }
 };
 
-function secondsToHms(d) {
- d = Number(d);
- var h = Math.floor(d / 3600);
- var m = Math.floor((d % 3600) / 60);
- var s = Math.floor((d % 3600) % 60);
-
- var hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
- var mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
- var sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
- return hDisplay + mDisplay + sDisplay;
-}
-
-bot(
- {
-  on: "text",
-  fromMe: false,
- },
- async (message, match) => {
-  const afkText = "I'm currently AFK.";
-  const reasonText = AFK.reason !== false ? `\n*Reason:* \`\`\`${AFK.reason}\`\`\`` : "";
-  const lastSeenText = AFK.lastseen !== 0 ? `\n*Last seen:* \`\`\`${secondsToHms(Math.round(new Date().getTime() / 1000) - AFK.lastseen)} ago\`\`\`` : "";
-
-  if (AFK.isAfk && (!message.jid.includes("@g.us") || (message.jid.includes("@g.us") && (message.mention?.length !== 0 || message.reply_message !== false)))) {
-   if (message.jid.includes("@g.us") && message.mention?.length !== 0) {
-    message.mention.map(async (jid) => {
-     if (message.client.user.jid.split("@")[0] === jid.split("@")[0]) {
-      await message.send(afkText + reasonText + lastSeenText, {
-       quoted: message.data,
-      });
-     }
-    });
-   } else if (message.jid.includes("@g.us") && message.reply_message !== false) {
-    if (message.reply_message.jid.split("@")[0] === message.client.user.jid.split("@")[0]) {
-     await message.send(afkText + reasonText + lastSeenText, {
-      quoted: message.data,
-     });
-    }
-   } else {
-    await message.send(afkText + reasonText + lastSeenText, {
-     quoted: message.data,
-    });
-   }
-  }
+db.update = async (newData) => {
+ try {
+  const filePath = "./afk.json";
+  const currentData = await db.get();
+  const updatedData = { ...currentData, ...newData };
+  fs.writeFileSync(filePath, JSON.stringify(updatedData, null, 2), "utf-8");
+  return updatedData;
+ } catch (error) {
+  console.error("Error updating data:", error);
  }
-);
+};
 
-bot(
- {
-  on: "text",
-  fromMe: true,
- },
- async (message, match) => {
-  if (AFK.isAfk && !message.id.startsWith("3EB0")) {
-   AFK.lastseen = 0;
-   AFK.reason = false;
-   AFK.isAfk = false;
-   await message.send("You are no longer AFK.");
-  }
- }
-);
+let afk = false;
+
+const afkMessage = `
+🌟 Hey there! I'm currently away from my keyboard. 🌟
+
+Feel free to leave a message, and I'll get back to you as soon as I can.
+
+Thank you for your patience! 😊
+
+🕒 Last seen: @lastseen
+🔔 I'll be back soon!
+
+Take care! 💻
+`;
 
 bot(
  {
   pattern: "afk",
-  fromMe: true,
-  desc: "Set your status as AFK.",
+  desc: "away from keyboard",
+  category: "chats",
  },
- async (message, match) => {
-  if (!AFK.isAfk) {
-   AFK.lastseen = Math.round(new Date().getTime() / 1000);
-   if (match !== "") {
-    AFK.reason = match;
-   }
-   AFK.isAfk = true;
+ async (message) => {
+  try {
+   let data = await db.get();
+   afk = data.afk || { users: {} };
 
-   await message.send("You are now AFK." + (AFK.reason !== false ? `\n*Reason:* \`\`\`${AFK.reason}\`\`\`` : ""));
+   afk[message.sender] = { lastseen: new Date() };
+   data.afk = { ...afk };
+   data = await db.update(data);
+
+   if (data) {
+    const response = afkMessage.replace("@lastseen", getTimeDifference(afk[message.sender].lastseen));
+    await message.reply(response);
+   } else {
+    message.reply("*Request Denied!*");
+   }
+  } catch (error) {
+   message.error(`${error}\n\nCommand: AFK`, error);
+  }
+ }
+);
+
+bot(
+ {
+  pattern: "unafk",
+  desc: "turn off away from keyboard",
+  category: "chats",
+ },
+ async (message) => {
+  try {
+   let data = await db.get();
+   afk = data.afk || {};
+
+   if (!afk[message.sender]) {
+    return message.reply("*You are not AFK.*");
+   }
+
+   delete afk[message.sender];
+   data.afk = { ...afk };
+   data = await db.update(data);
+
+   if (data) {
+    await message.reply("Finally, You are back!");
+   } else {
+    message.reply("*Request Denied!*");
+   }
+  } catch (error) {
+   message.error(`${error}\n\nCommand: UnAFK`, error, "ERROR");
+  }
+ }
+);
+
+const messages = {
+ 2: "*Hey I already informed you!*\n",
+ 3: "*Stop spamming!*",
+};
+
+function getTimeDifference(previousTime) {
+ const previousDate = new Date(previousTime);
+ const currentDate = new Date();
+ const timeDifference = currentDate - previousDate;
+ const days = Math.floor(timeDifference / 86400000);
+ const hours = Math.floor((timeDifference % 86400000) / 3600000);
+ const minutes = Math.floor((timeDifference % 3600000) / 60000);
+
+ return `${days ? "Days " + days + ", " : ""}Hours ${hours || 0}, Minutes ${minutes || 0}`;
+}
+
+bot(
+ {
+  on: "main",
+ },
+ async (message) => {
+  if (message.fromMe || message.isBot) return;
+
+  try {
+   if (!afk) {
+    let data = await db.get();
+    afk = data.afk || { users: [] };
+   }
+
+   const isReplyFromMe = message.reply_message && message.reply_message.fromMe;
+   let mentionedUsers = message.mentionedJid[0] ? [...message.mentionedJid] : [];
+
+   if (message.reply_message) {
+    mentionedUsers.push(message.reply_message.sender);
+   }
+
+   for (const user of mentionedUsers) {
+    if (afk[user] && message.sender !== user) {
+     afk[user].users[message.sender] = (afk[user].users[message.sender] || 0) + 1;
+
+     if (afk[user].users[message.sender] > 3) continue;
+
+     const warning = messages[afk[user].users[message.sender]];
+     const response = afkMessage.replace("@lastseen", `Last seen: ${getTimeDifference(afk[user].lastseen)}`);
+
+     await message.reply(response);
+    }
+   }
+  } catch (error) {
+   console.log("ERROR IN AFK MAIN\n", error);
   }
  }
 );
