f3b45cef33b61711eb18753c84170552ae584838
Astro (Daniel) <danielfx011@gmail.com>
2024-07-18 09:06:40 +0200

    patch v2

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-//#ENJOY
+const toBool = (x) => x == "true";
+const { Sequelize } = require("sequelize");
+const { existsSync } = require("fs");
+const path = require("path");
+const configPath = path.join(__dirname, "./config.env");
+const databasePath = path.join(__dirname, "./database.db");
 const fs = require("fs-extra");
-if (fs.existsSync(".env"))
-  require("dotenv").config({ path: __dirname + "/.env" });
-global.audio = "";
-global.video = "";
+if (fs.existsSync(".env")) require("dotenv").config({ path: __dirname + "/.env" });
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
 global.email = "astromedia0010@gmail.com";
@@ -12,7 +14,7 @@ global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
-global.timezone =  process.env.TIME_ZONE || "Africa/Lagos";
+global.timezone = process.env.TIME_ZONE || "Africa/Lagos";
 global.github = process.env.GITHUB || "https://github.com/Astropeda/Asta-Md";
 global.gurl = process.env.URL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
 global.website = process.env.WEBSITE || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
@@ -25,53 +27,41 @@ global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
-global.disablegroup = process.env.DISABLE_GROUPS || "false",
-global.MsgsInLog = process.env.MSGS_IN_LOG || "true";
+(global.disablegroup = process.env.DISABLE_GROUPS || "false"), (global.MsgsInLog = process.env.MSGS_IN_LOG || "true");
 global.userImages = process.env.USER_IMAGES || "";
 global.waPresence = process.env.WAPRESENCE || "online";
-global.readcmds = process.env.READ_COMMAND || "false";
-global.readmessage = process.env.READ_MESSAGE || "false";
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "false";
-global.save_status = process.env.AUTO_SAVE_STATUS || "false";
-global.save_status_from = process.env.SAVE_STATUS_FROM || "";
-global.read_status_from = process.env.READ_STATUS_FROM || "";
 
-global.api_smd = "https://api-smd-1.vercel.app";
-global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
-global.SESSION_ID =
-  process.env.SESSION_ID ||
-  ""
+global.SESSION_ID = process.env.SESSION_ID || "";
 module.exports = {
-  menu: process.env.MENU || "",
-  HANDLERS: process.env.PREFIX || "/",
-  BRANCH: process.env.BRANCH || "main",
-  VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "©ᴀsᴛᴀ ᴍᴅ",
-  author: process.env.PACK_AUTHER || "ᴀsᴛᴀ ᴍᴅ",
-  packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "ᴀsᴛᴀ ᴍᴅ",
-  ownername: process.env.OWNER_NAME || "ᴀsᴛᴀ",
-  errorChat: process.env.ERROR_CHAT || "",
-  KOYEB_API: process.env.KOYEB_API || "false",
-  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
-  antilink_values: process.env.ANTILINK_VALUES || "all",
-  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
-  aitts_Voice_Id: process.env.AITTS_ID || "37",
-  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
-  LANG: (process.env.THEME || "ASTA").toUpperCase(),
+ menu: process.env.MENU || "",
+ HANDLERS: process.env.PREFIX || "/",
+ BRANCH: process.env.BRANCH || "main",
+ VERSION: process.env.VERSION || "1.0.0",
+ caption: process.env.CAPTION || "©ᴀsᴛᴀ ᴍᴅ",
+ author: process.env.PACK_AUTHER || "ᴀsᴛᴀ ᴍᴅ",
+ packname: process.env.PACK_NAME || "♥️",
+ botname: process.env.BOT_NAME || "ᴀsᴛᴀ ᴍᴅ",
+ ownername: process.env.OWNER_NAME || "ᴀsᴛᴀ",
+ errorChat: process.env.ERROR_CHAT || "",
+ KOYEB_API: process.env.KOYEB_API || "false",
+ REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+ OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+ HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+ HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+ antilink_values: process.env.ANTILINK_VALUES || "all",
+ HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+ aitts_Voice_Id: process.env.AITTS_ID || "37",
+ ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+ WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+ LANG: (process.env.THEME || "ASTA").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
 let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-  fs.unwatchFile(file);
-  console.log(`Update'${__filename}'`);
-  delete require.cache[file];
-  require(file);
+ fs.unwatchFile(file);
+ console.log(`Update'${__filename}'`);
+ delete require.cache[file];
+ require(file);
 });
