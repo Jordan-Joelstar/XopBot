0c9a95c17f189c4ce22e4bbc28b3a1ffac34229b
Astro (Daniel) <danielfx011@gmail.com>
2024-07-19 09:56:27 +0200

    runtime & buttons

---
+++ plugins/buttons.js
@@ -0,0 +1,254 @@
+const os = require('os')
+const fs = require('fs')
+const Config = require('../config')
+const { fancytext, runtime, formatp, prefix, bot } = require('../lib')
+const longCharacter = String.fromCharCode(8206)
+const readMoreSeparator = longCharacter.repeat(4001)
+const cron = require('node-cron')
+
+global.caption = global.caption || Config.caption || 'αѕтα-м∂ 2024'
+global.ownerName = global.ownerName || Config.ownerName || 'αѕтяσ'
+global.botName = global.botName || Config.botName || 'αѕтα-м∂'
+global.menuOption = global.menuOption || Config.menu || ''
+global.commandPrefix = global.commandPrefix || Config.HANDLERS || prefix || '^'
+global.menuStyle = global.menuStyle || process.env.MENU_STYLE || 'ss'
+global.uiCache = {}
+global.uiUrls = []
+let cronStarted = false
+
+if (!cronStarted) {
+ cron.schedule('*/15 * * * *', () => {
+  cronStarted = true
+  fs.readdir('./temp', (err, files) => {
+   if (err) return
+   files.forEach((file) => {
+    try {
+     fs.unlinkSync(`./temp/${file}`)
+    } catch (e) {
+     console.log('Error deleting files:', e)
+    }
+   })
+  })
+ })
+}
+
+global.createUserInterface = () => {
+ if (!global.userImages || /text|txt|nothing/.test(global.userImages)) {
+  return {}
+ }
+ const imageFormats = ['.jpg', '.jpeg', '.png', '.webp']
+ const videoFormats = ['.mp4', '.avi', '.mov', '.mkv', '.gif', '.m4v']
+ if (!uiUrls || !uiUrls[0]) {
+  uiUrls = global.userImages ? global.userImages.split(',') : ['']
+  uiUrls = uiUrls.filter((url) => url.trim() !== '')
+ }
+ const selectedUrl = (uiUrls[Math.floor(Math.random() * uiUrls.length)] || '').trim()
+ if (/http/gi.test(selectedUrl) && !uiCache[selectedUrl]) {
+  const fileExtension = selectedUrl.substring(selectedUrl.lastIndexOf('.')).toLowerCase()
+  if (imageFormats.includes(fileExtension)) {
+   uiCache[selectedUrl] = 'image'
+  } else if (videoFormats.includes(fileExtension)) {
+   uiCache[selectedUrl] = 'video'
+  }
+ }
+ return {
+  [uiCache[selectedUrl] || 'Invalid_Type_URL']: {
+   url: selectedUrl,
+  },
+ }
+}
+
+global.createButtons = (inputMessage) => {
+ if (!inputMessage || Array.isArray(inputMessage)) {
+  return inputMessage || []
+ }
+ const buttonRegex = /#button\s*:\s*([^|]+)\s*\|\s*display_text\s*:\s*([^|]+)(?:\s*\|\s*(id)\s*:\s*([^|]+))?(?:\s*\|\s*(copy_code)\s*:\s*([^|]+))?\/#/gi
+ const buttonsArray = []
+ let buttonMatch
+ while ((buttonMatch = buttonRegex.exec(inputMessage)) !== null) {
+  try {
+   const buttonType = buttonMatch[1].trim()
+   const displayText = buttonMatch[2].trim()
+   const buttonId = buttonMatch[4] ? buttonMatch[4].trim() : ''
+   const copyCode = buttonMatch[6] ? buttonMatch[6].trim() : ''
+   let buttonObject = { display_text: displayText }
+
+   if (buttonType === 'cta_copy') {
+    buttonObject = { display_text: displayText, id: buttonId, copy_code: copyCode }
+   } else if (buttonType === 'cta_url') {
+    buttonObject = {
+     display_text: displayText,
+     url: (buttonId || '').replace(' /#', '').trim(),
+     merchant_url: copyCode || 'https://www.google.com',
+    }
+   } else {
+    buttonObject = { display_text: displayText, id: buttonId }
+   }
+
+   if (buttonType) {
+    buttonsArray.push({
+     name: buttonType,
+     buttonParamsJson: JSON.stringify(buttonObject),
+    })
+   } else {
+    console.log('Button name missing in', buttonMatch[0])
+   }
+  } catch (error) {
+   console.log(error)
+  }
+ }
+ return buttonsArray || []
+}
+
+global.sendButtons = async (message, context = {}, messageBody = [], customJid = false) => {
+ if (!message) {
+  throw 'Message instance is required.'
+ }
+ const recipientJid = customJid || message.jid
+ if (typeof context !== 'object') {
+  context = {}
+ }
+ context.messageId = context.messageId || message.bot.messageId()
+ if (typeof messageBody === 'string') {
+  messageBody = createButtons(messageBody)
+ }
+ if (typeof context.buttons === 'string' || Array.isArray(context.buttons)) {
+  messageBody = [...messageBody, ...(createButtons(context.buttons) || [])]
+ }
+ const { generateWAMessageFromContent, proto, prepareWAMessageMedia } = require('@whiskeysockets/baileys')
+ let mediaMessageData = {}
+ try {
+  if (typeof context.imageMessage === 'object') {
+   mediaMessageData = { imageMessage: context.imageMessage }
+  } else if (typeof context.videoMessage === 'object') {
+   mediaMessageData = { videoMessage: context.videoMessage }
+  } else {
+   let mediaMessage = false
+   const mediaContext = context.image || context.video ? context : createUserInterface()
+   if (mediaContext.image) {
+    mediaMessage = (await prepareWAMessageMedia({ image: mediaContext.image || null }, { upload: message.bot.waUploadToServer })) || false
+   } else if (mediaContext.video) {
+    mediaMessage = (await prepareWAMessageMedia({ video: mediaContext.video || null }, { upload: message.bot.waUploadToServer })) || false
+   }
+   if (mediaMessage) {
+    mediaMessageData = mediaMessage.imageMessage ? { imageMessage: mediaMessage.imageMessage } : mediaMessage.videoMessage ? { videoMessage: mediaMessage.videoMessage } : {}
+   }
+  }
+ } catch (error) {
+  mediaMessageData = {}
+ }
+ const enhancedContext = {
+  ...(await message.bot.contextInfo(botName, message.senderName || ownerName)),
+  ...(context.contextInfo || {}),
+ }
+ const formattedMessage = generateWAMessageFromContent(
+  recipientJid,
+  {
+   viewOnceMessage: {
+    message: {
+     interactiveMessage: proto.Message.InteractiveMessage.create({
+      body: { text: context.text || context.body || context.caption || 'Astro' },
+      footer: { text: context.footer || 'αѕтα тє¢н тєαм' },
+      header: {
+       ...(mediaMessageData || {}),
+       hasMediaAttachment: mediaMessageData.imageMessage || mediaMessageData.videoMessage ? true : false,
+       ...(context.header || {}),
+      },
+      contextInfo: enhancedContext,
+      nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+       buttons: messageBody,
+      }),
+     }),
+     messageContextInfo: {
+      deviceListMetadata: {},
+      deviceListMetadataVersion: 2,
+     },
+    },
+   },
+  },
+  context
+ )
+ await message.bot.relayMessage(recipientJid, formattedMessage.message, { messageId: context.messageId })
+ return formattedMessage
+}
+
+bot(
+ {
+  cmdname: 'menu',
+  desc: 'Help list',
+  type: 'user',
+ },
+ async (message, match) => {
+  try {
+   const { commands } = require('../lib')
+
+   let menuHeader, menuSubHeader, menuFooter, categoryHeaderStart, categoryHeaderEnd, commandLine, categoryFooter
+   let menuVersion = 1 // Choose one menu style
+   if (menuOption === '') {
+    menuVersion = Math.floor(Math.random() * 4) + 1
+   }
+   if (menuVersion === 1 || menuOption.trim().startsWith('1') || menuOption.toLowerCase().includes('v1')) {
+    menuHeader = '╭━━━〔 *' + botName + '* 〕━━━┈⊷'
+    menuSubHeader = '┃✵│'
+    menuFooter = '┃✵╰──────────────\n╰━━━━━━━━━━━━━━━┈⊷'
+    categoryHeaderStart = '╭─────────────┈⊷\n│「'
+    categoryHeaderEnd = '」\n╰┬────────────┈⊷'
+    commandLine = '││◦➛'
+    categoryFooter = '│╰────────────┈⊷\n╰─────────────┈⊷'
+   }
+
+   const commandGroups = {}
+   commands.forEach((cmd) => {
+    if (!cmd.dontAddCommandList && cmd.pattern) {
+     if (!commandGroups[cmd.category]) {
+      commandGroups[cmd.category] = []
+     }
+     commandGroups[cmd.category].push(cmd.pattern)
+    }
+   })
+
+   const menuFancyOptions = [1, 22, 23, 1, 36, 35, 48, 1, 42, 55, 56]
+   const fancyStyle = parseInt(menuStyle || '') || menuFancyOptions[Math.floor(Math.random() * menuFancyOptions.length)]
+
+   let menuContent = menuHeader + '\n' + menuSubHeader + (menuVersion === 3 ? ` ᴜᴘᴛɪᴍᴇ: ${runtime(process.uptime())}\n` : '')
+   const osStatus = `${botName.toUpperCase()} ${os.platform()} [${formatp(os.totalmem() - os.freemem())}/${formatp(os.totalmem())}]`.trim()
+   if (menuVersion !== 1) {
+    menuContent += menuSubHeader + '│' + menuSubHeader + (menuVersion === 4 ? `${botName} ${menuOption}` : osStatus) + '\n'
+   }
+   menuContent += menuSubHeader + '✵╭───⊷╮\n' + menuSubHeader + '✵│ Owner: ' + ownerName + '\n' + menuSubHeader + '✵╰───⊷╯'
+   for (const category in commandGroups) {
+    menuContent += '\n' + categoryHeaderStart + ' ' + category + ' ' + categoryHeaderEnd
+    commandGroups[category].forEach((pattern) => {
+     menuContent += '\n' + commandLine + ' ' + commandPrefix + pattern
+    })
+    menuContent += '\n' + categoryFooter
+   }
+   menuContent += '\n' + menuFooter
+   menuContent = menuContent.replace(/"/g, '「').replace(/"/g, '」').trim()
+   menuContent = await fancytext(menuContent, fancyStyle)
+   const menuContext = {
+    text: menuContent,
+    contextInfo: {},
+   }
+   await sendButtons(message, menuContext, [{ name: 'cta_url', buttonParamsJson: JSON.stringify({ display_text: 'Owner', url: Config.LINK }) }])
+  } catch (error) {
+   console.log(error)
+  }
+ }
+)
+
+bot(
+ {
+  cmdname: 'menu2',
+  desc: 'Menu buttons test',
+  type: 'user',
+ },
+ async (message, match) => {
+  try {
+   const buttonsTestMessage = 'Testing #button : cta_url | display_text : Visit Us | url : https://example.com/#'
+   await sendButtons(message, {}, buttonsTestMessage)
+  } catch (error) {
+   console.log(error)
+  }
+ }
+)
--- plugins/system.js
+++ plugins/system.js
@@ -1,135 +1,135 @@
-const fs = require("fs");
-const { exec } = require("child_process");
-const { plugins, bot } = require("../lib");
-const isOwner = true;
+const fs = require('fs')
+const { exec } = require('child_process')
+const { plugins, bot, runtime } = require('../lib')
+const isOwner = true
 
 bot(
  {
-  pattern: "shutdown",
-  info: "To shutdown bot",
-  type: "tools",
+  pattern: 'shutdown',
+  info: 'To shutdown bot',
+  type: 'tools',
   fromMe: isOwner,
  },
  async (message) => {
-  message.reply("Shutting Down");
-  exec("pm2 stop all");
+  message.reply('Shutting Down')
+  exec('pm2 stop all')
  }
-);
+)
 
 bot(
  {
-  pattern: "restart",
-  info: "To restart bot",
-  type: "tools",
+  pattern: 'restart',
+  info: 'To restart bot',
+  type: 'tools',
   fromMe: isOwner,
  },
  async (message) => {
-  message.reply("Restarting");
-  exec("pm2 restart all");
+  message.reply('Restarting')
+  exec('pm2 restart all')
  }
-);
+)
 
 bot(
  {
-  pattern: "plugins",
-  type: "tools",
-  info: "Shows list of all externally installed modules",
+  pattern: 'plugins',
+  type: 'tools',
+  info: 'Shows list of all externally installed modules',
   fromMe: isOwner,
  },
  async (message, args) => {
   try {
-   const pluginList = await plugins(message, "plugins", args);
-   const response = !pluginList ? `*_There's no plugin installed_*` : !args ? `*All Installed Modules are:*\n\n${pluginList}` : pluginList;
-   await message.send(response);
+   const pluginList = await plugins(message, 'plugins', args)
+   const response = !pluginList ? `*_There's no plugin installed_*` : !args ? `*All Installed Modules are:*\n\n${pluginList}` : pluginList
+   await message.send(response)
   } catch (error) {
-   message.error(`${error} \n\ncmdName plugins\n`);
+   message.error(`${error} \n\ncmdName plugins\n`)
   }
  }
-);
+)
 
 bot(
  {
-  pattern: "remove",
-  type: "tools",
-  info: "Removes external modules.",
+  pattern: 'remove',
+  type: 'tools',
+  info: 'Removes external modules.',
   fromMe: isOwner,
  },
  async (message, args) => {
   if (!args) {
-   return await message.reply("*_Provide Plugin Name_*");
+   return await message.reply('*_Provide Plugin Name_*')
   }
 
-  if (args === "all") {
-   return await message.reply(await plugins("remove", "all", __dirname));
+  if (args === 'all') {
+   return await message.reply(await plugins('remove', 'all', __dirname))
   }
 
   try {
-   const result = await plugins(message, "remove", args, __dirname);
-   await message.send(result);
+   const result = await plugins(message, 'remove', args, __dirname)
+   await message.send(result)
   } catch (error) {
-   message.error(`Error removing plugin: ${error}`);
+   message.error(`Error removing plugin: ${error}`)
   }
  }
-);
+)
 
 bot(
  {
-  pattern: "install",
-  type: "tools",
-  info: "Installs external modules.",
+  pattern: 'install',
+  type: 'tools',
+  info: 'Installs external modules.',
   fromMe: isOwner,
  },
  async (message, args) => {
-  const url = args || (message.quoted ? message.quoted.text : "");
+  const url = args || (message.quoted ? message.quoted.text : '')
 
-  if (!url.toLowerCase().includes("https")) {
-   return await message.send("*_Provide Plugin URL_*");
+  if (!url.toLowerCase().includes('https')) {
+   return await message.send('*_Provide Plugin URL_*')
   }
 
-  const result = await plugins(message, "install", url, __dirname);
-  await message.reply(result);
+  const result = await plugins(message, 'install', url, __dirname)
+  await message.reply(result)
  }
-);
+)
 
 bot(
  {
-  pattern: "cache",
-  type: "tools",
-  info: "Clear temporary files cache",
+  pattern: 'cache',
+  type: 'tools',
+  info: 'Clear temporary files cache',
  },
  async (message) => {
   try {
-   const tempDir = "./temp";
+   const tempDir = './temp'
    if (fs.existsSync(tempDir)) {
-    fs.readdirSync(tempDir).forEach((file) => fs.rmSync(`${tempDir}/${file}`));
+    fs.readdirSync(tempDir).forEach((file) => fs.rmSync(`${tempDir}/${file}`))
    }
-   await message.reply("Cache Cleared From Bot_");
+   await message.reply('Cache Cleared From Bot_')
   } catch (error) {
-   message.error(`${error}\n\nCommand: cleartmp`, error, false);
+   message.error(`${error}\n\nCommand: cleartmp`, error, false)
   }
  }
-);
+)
 
 bot(
  {
-  pattern: "issue",
-  desc: "Report bug/features of bot to its creator!",
-  type: "tools",
+  pattern: 'issue',
+  desc: 'Report bug/features of bot to its creator!',
+  type: 'tools',
  },
  async (message, args) => {
   try {
    if (!args) {
-    return message.reply("_Write Your Bot Issue!_");
+    return message.reply('_Write Your Bot Issue!_')
    }
-   if (args.split(" ").length < 10) {
-    return message.reply("_Write Atleast 10 Words_");
+   if (args.split(' ').length < 10) {
+    return message.reply('_Write Atleast 10 Words_')
    }
 
-   const header = "*XPO BOT ISSUE REPORT*";
-   const body = `\n\n*User*: @${message.senderNum}\n\n*ISSUES PAGE*: ${args}`;
-   const footer = `\n\n_${message.senderName.split("\n").join(" ")}, Issues sent!_`;
+   const header = '*XPO BOT ISSUE REPORT*'
+   const body = `\n\n*User*: @${message.senderNum}\n\n*ISSUES PAGE*: ${args}`
+   const footer = `\n\n_${message.senderName.split('\n').join(' ')}, Issues sent!_`
    await message.sendMessage(
-    "2348039607375@s.whatsapp.net",
+    '2348039607375@s.whatsapp.net',
     {
      text: header + body,
      mentions: [message.sender],
@@ -137,16 +137,33 @@ bot(
     {
      quoted: message,
     }
-   );
+   )
    await message.reply(
     header + footer + body,
     {
      mentions: [message.sender],
     },
     message
-   );
+   )
   } catch (error) {
-   message.error(`${error}\n\nCommand: request`, error, false);
+   message.error(`${error}\n\nCommand: request`, error, false)
   }
  }
-);
+)
+
+bot({
+ name: 'uptime',
+ alias: ['runtime'],
+ type: 'tools',
+ desc: 'Get bot runtime information',
+ async execute(message, args) {
+  try {
+   const uptime = runtime(process.uptime())
+   const response = `*Bot Uptime: ${uptime}*`
+   await message.reply(response)
+  } catch (error) {
+   console.error('Error in uptime command:', error)
+   await message.reply('An error occurred while fetching uptime information.')
+  }
+ },
+})
